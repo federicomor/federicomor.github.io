fe7916712cc8fdcc2d55739e78cb47b1b38bc36c
federicomor <66522155+federicomor@users.noreply.github.com>
2024-06-13 16:06:13 +0000

    Deploying to gh-pages from  @ 56584b2c16c83f38ff7f57fa7ef67d81ff901aff 🚀

---
+++ assets/scripts/Activity/paradosso_compleanni.jl
@@ -0,0 +1,15 @@
+using Plots
+using Base: basename # for easy filenames save
+
+plotly()
+PlotlyKaleido.start() # needed for all savefig with plotly
+
+p1(n) = 1 - factorial(big(365)) / (factorial(big(365-n)) * big(365)^n)
+p1_values = p1.(1:365)
+plot(p1_values,title="p1 values")
+savefig("figs/$(splitext(basename(@__FILE__))[1])_1.json")
+
+p2(n) = 1 - (big(364/365)^n)
+p2_values = p2.(1:2000)
+plot(p2_values,title="p2 values")
+savefig("figs/$(splitext(basename(@__FILE__))[1])_2.json")
