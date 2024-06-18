5cfa8b86f296bad93976ea20bdd2a9922976da52
federicomor <66522155+federicomor@users.noreply.github.com>
2024-06-18 08:08:05 +0000

    Deploying to gh-pages from  @ 372f0f8b90cadaa7b5cf697fef30aa0dd9444eae 🚀

--- youtube_videos.jl
+++
@@ -1,6 +0,0 @@
-videos = Dict(
-              "course-intro" => "vxjRWtWoD_w",
-              "installation" => "OOjKEgbt8AI",
-              "array-intro"  => "CwDI-YOjWhc",
-              "cute-one"     => "foN1_hAGfNg",
-              )
