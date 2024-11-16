8d15b3fe2ec7ccd4639e8d71d7e19fc1fc1d7a26
federicomor <66522155+federicomor@users.noreply.github.com>
2024-11-16 14:47:14 +0000

    Deploying to gh-pages from  @ 828241c1ef59d5baa20b02e83e692821297c38eb 🚀

--- assets/scripts/Activity/tennis_tiebreak_prob.jl
+++ assets/scripts/Activity/tennis_tiebreak_prob.jl
@@ -27,8 +27,8 @@ function ptiebreak(a::Float64,b::Float64)
     return ptot
 end
 
-aa = range(0.01,0.99, length=40)
-bb = range(0.01,0.99, length=40)
+aa = range(0.01,0.99, step=0.01)
+bb = range(0.01,0.99, step=0.01)
 p = surface(aa, bb, ptiebreak,camera=(25,20))
 xlabel!("a values")
 ylabel!("b values")
