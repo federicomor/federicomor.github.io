17d125b6da58f98e42ee9193875894c38f7d1a2c
federicomor <66522155+federicomor@users.noreply.github.com>
2025-02-08 17:16:47 +0000

    Deploying to gh-pages from  @ 10f3ef6c073ad311c3dea4ed4650d9a187b20849 🚀

--- activity/project_euler/p_eulers.jl
+++ activity/project_euler/p_eulers.jl
@@ -1,9 +1,23 @@
-sum([i for i in 1:999 if (i%3==0 || i%5==0)])
+# Find the difference between 
+# - the sum of the squares (A)
+# - and the square of the sum (B)
+# of the first one hundred natural numbers
+# quindi B-A
 
-S = 0
-for i in 1:999
-	if i%3 == 0 || i%5 == 0
-		S += i
-	end
+# one liner
+D = (sum([i for i in 1:100]))^2 - sum([i^2 for i in 1:100])
+
+# a mano
+cur_sum = 0
+cur_sum_sq = 0
+for i in 1:100
+	cur_sum += i
+	cur_sum_sq += i^2
 end
-@show S
+D = cur_sum^2 - cur_sum_sq
+
+
+# metodo matematico
+# D = (∑i)^2 - (∑i^2)
+n = 100
+D = ( n*(n+1)/2 )^2 - n*(n+1)*(2n+1)/6
