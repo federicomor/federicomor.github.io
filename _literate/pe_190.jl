df581ca5db641a23dc8edd678a48745904ac4781
Federico Angelo Mor <federangelomor@gmail.com>
2025-04-07 20:45:08 +0200

    Update pe_190.jl

--- _literate/pe_190.jl
+++ _literate/pe_190.jl
@@ -1,12 +1,12 @@
 # ### Esplorazione del problema
-# Come sempre, per approcciare un problema complicato, possiamo partire scalando il problema in piccolo e osservare cosa accade, per esempio, nei casi di $m=2$ e $m=3$.
-# Giusto perché è carino, possiamo anche raccontare graficamente questa prima esplorazione.
+# Come sempre, per approcciare un problema complicato, possiamo partire in piccolo e osservare cosa accade in casi più semplici; quindi qui, per esempio, nei casi di $m=2$ e $m=3$.
+# Giusto perché è carino, e perché Julia permette di farlo molto facilmente, possiamo anche raccontare graficamente questa prima esplorazione.
 using Plots 
 using JSON
 plotlyjs()
 xs(m) = [(2i/(m+1)) for i in 1:m]; #hide
 #-
-# Nel caso di $m=2$ questo è quello che otterremo:
+# Questo è quello che si osserva nel caso di $m=2$:
 nout = 100
 x1s = range(0,2,length=nout)
 vals = zeros(nout)
@@ -28,7 +28,7 @@ println("Max value ($(maximum(vals))) reached at index $max_idx, ie for")
 println("x1 = ", x1_max)
 println("x2 = ", 2-x1_max)
 #- 
-# Vera soluzione:
+# vera soluzione:
 xs(2) # hide
 #-
 # mentre questo per $m=3$:
@@ -60,14 +60,14 @@ println("x1 = ", x1_max)
 println("x2 = ", x2_max)
 println("x3 = ", 3-x1_max-x2_max)
 #- 
-# Vera soluzione:
+# vera soluzione:
 xs(3) # hide
 #-
 
-# Questo approccio, come detto, può servire per ambientarci nel problema, e sarebbe ad esempio utile se potessimo già da qui derivare o intuire un qualche pattern rilevante. Tuttavia non sembra molto il caso, dai numeri derivati qui sopra, quindi possiamo passare direttamente alla trattazione matematica e rigorosa del problema.
+# Questo approccio, come detto, può servire per ambientarci nel problema, e sarebbe ad esempio utile se potessimo già da questi esempi derivare o intuire un qualche pattern rilevante. Tuttavia, non sembra molto il caso dai numeri derivati qui sopra, quindi possiamo passare direttamente alla trattazione matematica e rigorosa del problema.
 
 # ### Soluzione effettiva
-# L'idea è che stiamo affrontando un problema di ottimizzazione per una funzione multivariata $f({\bf x}) : \R^n\to \R$, soggetta al vincolo $g({\bf x})=0$, dove anche $g({\bf x}): \R^n\to \R$: un contesto che sembra proprio chiamare in causa i moltiplicatori di Lagrange! Quando ci ho pensato non volevo crederci, e soprattutto non volevo rimestare nel torbido per recuperare i miei appunti di Analisi 2. Tuttavia, a malincuore, ho dovuto farlo, e in effetti con quelli si arriva tranquillamente (e anche molto velocemente) alla soluzione.
+# L'idea è che stiamo affrontando un problema di ottimizzazione per una funzione multivariata $f({\bf x}) : \R^n\to \R$, soggetta al vincolo $g({\bf x})=0$, dove anche $g({\bf x}): \R^n\to \R$: un contesto che sembra proprio chiamare in causa i moltiplicatori di Lagrange! Quando ci ho pensato non volevo crederci, e soprattutto non volevo rimestare nel torbido per recuperare i miei appunti di Analisi 2. A malincuore ho dovuto però farlo, e in effetti con quelli si arriva tranquillamente (e anche molto velocemente) alla soluzione.
 
 # I moltiplicatori di Lagrange permettono infatti di convertire un problema di ottimizzazione (inteso come ricerca di punti di massimo/minimo) di una funzione $f$ soggetta a un vincolo[^1] $g$ in un altro problema di ottimizzazione, stavolta _libero_, quindi non soggetto a vincoli, di un'altra funzione $\mathcal{L}$, al costo di aggiungere una variabile $\lambda$. Questa nuova funzione sarebbe $$\mathcal{L}({\bf x},\lambda) = f({\bf x}) + \lambda g({\bf x})$$ 
 # i cui punti di massimo/minimo sono molto facili da individuare: basta infatti porre la "derivata uguale a zero", che in questo contesto multivariato si traduce nel chiedere che il gradiente della funzione sia uguale a zero, ovvero $\nabla \mathcal{L}({\bf x},\lambda) = {\bf 0}$. Questa richiesta, vista la definizione di $\mathcal{L}$, equivale in realtà a $$\label{system}\begin{cases}\nabla f({\bf x}) = -\lambda g({\bf x}) \\ g({\bf x}) = 0\end{cases}$$
